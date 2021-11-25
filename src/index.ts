87d610a5f00e7a0c1788e1a1d194b056c7140cb1
Brian Soule <briansoule4@gmail.com>
2021-11-25 17:52:58 -0600

    Added EVM Compatible Chains

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,6 @@ import { ChainID, isValidAddress } from './flow/index';
 import { groestl_2 }  from './groestl-hash-js/index';
 import { xmrAddressDecoder, xmrAddressEncoder } from './monero/xmr-base58';
 import { nimqDecoder, nimqEncoder } from './nimq';
-const SLIP44_MSB = 0x80000000
 
 type EnCoder = (data: Buffer) => string;
 type DeCoder = (data: string) => Buffer;
@@ -484,18 +483,6 @@ const hexChecksumChain = (name: string, coinType: number, chainId?: number) => (
   coinType,
   decoder: makeChecksummedHexDecoder(chainId),
   encoder: makeChecksummedHexEncoder(chainId),
-  name,
-});
-
-/* tslint:disable:no-bitwise */
-export const convertEVMChainIdToCoinType = (chainId: number) =>{
-  return Math.abs(SLIP44_MSB | chainId)
-}
-
-const evmChain = (name: string, coinType: number) => ({
-  coinType: convertEVMChainIdToCoinType(coinType),
-  decoder: makeChecksummedHexDecoder(),
-  encoder: makeChecksummedHexEncoder(),
   name,
 });
 
@@ -1465,7 +1452,6 @@ export const formats: IFormat[] = [
   getConfig('NANO', 165, nanoAddressEncoder, nanoAddressDecoder),
   bitcoinBase58Chain('RVN', 175, [[0x3c]], [[0x7a]]),
   hexChecksumChain('POA', 178),
-  bitcoinChain('LCC', 192, 'lcc', [[0x1c]], [[0x32], [0x05]]),
   eosioChain('EOS', 194, 'EOS'),
   getConfig('TRX', 195, bs58Encode, bs58Decode),
   getConfig('BCN', 204, bcnAddressEncoder, bcnAddressDecoder),
@@ -1495,7 +1481,6 @@ export const formats: IFormat[] = [
   bitcoinBase58Chain('CCA', 489, [[0x0b]], [[0x05]]),
   hexChecksumChain('THETA', 500),
   getConfig('SOL', 501, bs58EncodeNoCheck, bs58DecodeNoCheck),
-  hexChecksumChain('BSC', 519),
   getConfig('XHV', 535, xmrAddressEncoder, xmrAddressDecoder),
   getConfig('FLOW', 539, flowEncode, flowDecode),
   bech32Chain('IRIS', 566, 'iaa'),
@@ -1517,7 +1502,6 @@ export const formats: IFormat[] = [
   hexChecksumChain('TOMO', 889),
   getConfig('HNT', 904, hntAddresEncoder, hntAddressDecoder),
   bech32Chain('RUNE', 931, 'thor'),
-  hexChecksumChain('MATIC', 966),
   bitcoinChain('BCD', 999, 'bcd', [[0x00]], [[0x05]]),
   hexChecksumChain('TT', 1001),
   hexChecksumChain('FTM', 1007),
@@ -1548,6 +1532,13 @@ export const formats: IFormat[] = [
   bech32mChain('XCH', 8444, 'xch', 90),
   getConfig('NULS', 8964, nulsAddressEncoder, nulsAddressDecoder),
   bech32Chain('AVAX', 9000, 'avax'),
+  hexChecksumChain('ARB', 9001),
+  hexChecksumChain('BOBA', 9002),
+  hexChecksumChain('LOOP', 9003),
+  hexChecksumChain('STARK', 9004),
+//  Pending SLIP PR Merge https://github.com/satoshilabs/slips/pull/1202  
+//  hexChecksumChain('BSC', 9005),
+//  hexChecksumChain('AVAXC', 9006),
   hexChecksumChain('NRG', 9797),
   getConfig('ARDR', 16754, ardrAddressEncoder, ardrAddressDecoder),
   zcashChain('ZEL', 19167, 'za', [[0x1c, 0xb8]], [[0x1c, 0xbd]]),
@@ -1555,8 +1546,6 @@ export const formats: IFormat[] = [
   bitcoinBase58Chain('WICC', 99999, [[0x49]], [[0x33]]),
   getConfig('WAN', 5718350, wanChecksummedHexEncoder, wanChecksummedHexDecoder),
   getConfig('WAVES', 5741564, bs58EncodeNoCheck, wavesAddressDecoder),
-  // EVM chainIds have to be placed in descending order
-  evmChain('ARB1', 42161)
 ];
 
 export const formatsByName: { [key: string]: IFormat } = Object.assign({}, ...formats.map(x => ({ [x.name]: x })));
